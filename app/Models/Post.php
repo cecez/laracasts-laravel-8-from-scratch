49425f4f5e237802f4530dd56e57568e100ac6af
Cezar Rosa <cecez86@gmail.com>
2021-05-03 18:42:06 -0300

    - cacheando coleção de posts - ordenando listagem de posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,16 +32,25 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path('posts')))
-            ->map(function ($file) { return YamlFrontMatter::parseFile($file); })
-            ->map(function ($document) {
-                return new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                );
+        try {
+            return cache()->rememberForever('posts.all', function () {
+                return collect(File::files(resource_path('posts')))
+                    ->map(function ($file) {
+                        return YamlFrontMatter::parseFile($file);
+                    })
+                    ->map(function ($document) {
+                        return new Post(
+                            $document->title,
+                            $document->excerpt,
+                            $document->date,
+                            $document->body(),
+                            $document->slug
+                        );
+                    })
+                    ->sortByDesc('date');
             });
+        } catch (\Exception $e) {
+            abort(404);
+        }
     }
 }
