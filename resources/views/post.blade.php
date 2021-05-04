04ae6e86140db4a1578790f289a7938dad9624e3
Cezar Rosa <cecez86@gmail.com>
2021-05-04 18:34:17 -0300

    - componente para layout das páginas

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/app.css" rel="stylesheet">
-        <title>{{ $post->title }}</title>
-    </head>
-    <body>
+<x-layout>
         <article>
             <h1>{{ $post->title}}</h1>
             <div>
@@ -15,5 +7,4 @@
         </article>
 
         <a href="/">Voltar</a>
-    </body>
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/app.css" rel="stylesheet">
-        <title>Posts</title>
-    </head>
-    <body>
-
-        <article>
-            @foreach($posts as $post)
+<x-layout>
+        @foreach($posts as $post)
+            <article>
                 <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
                 <div>
                     {{ $post->excerpt }}
                 </div>
-            @endforeach
-        </article>
-
-    </body>
-</html>
+            </article>
+        @endforeach
+</x-layout>
