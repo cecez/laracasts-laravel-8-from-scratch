ffa0c88d8fa1e630cc389cd75bcee5add5e3c3cf
Cezar Rosa <cecez86@gmail.com>
2021-05-11 19:08:39 -0300

    - shifting to db approach instead of files - migration and model for posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,67 +1,11 @@
 <?php
 
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-
-class Post
+class Post extends Model
 {
-
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
-
-    public function __construct($title, $excerpt, $date, $body, $slug)
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function find($slug)
-    {
-        return self::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug) {
-        $posts = self::find($slug);
-
-        if (! $posts) {
-            throw new ModelNotFoundException();
-        }
-
-        return $posts;
-    }
-
-    public static function all()
-    {
-        try {
-            return cache()->rememberForever('posts.all', function () {
-                return collect(File::files(resource_path('posts')))
-                    ->map(function ($file) {
-                        return YamlFrontMatter::parseFile($file);
-                    })
-                    ->map(function ($document) {
-                        return new Post(
-                            $document->title,
-                            $document->excerpt,
-                            $document->date,
-                            $document->body(),
-                            $document->slug
-                        );
-                    })
-                    ->sortByDesc('date');
-            });
-        } catch (\Exception $e) {
-            abort(404);
-        }
-    }
+    use HasFactory;
 }
