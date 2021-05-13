6d563e71dff9ea4e209b15da815faa136d62a277
Cezar Rosa <cecez86@gmail.com>
2021-05-13 14:46:57 -0300

    - category model/migration and relationship with post - category link in post/posts pages

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,17 @@
 <x-layout>
         <article>
             <h1>{{ $post->title}}</h1>
+
+            <p>
+                <a href="#">{{ $post->category->name }}</a>
+            </p>
+
             <div>
                 {!! $post->body !!}
             </div>
         </article>
 
-        <a href="/">Voltar</a>
+        <p>
+            <a href="/">Voltar</a>
+        </p>
 </x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,6 +2,7 @@
         @foreach($posts as $post)
             <article>
                 <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+                <p><a href="#">{{ $post->category->name }}</a></p>
                 <div>
                     {{ $post->excerpt }}
                 </div>
