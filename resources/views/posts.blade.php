ffaf2037e5405f1daea17f1c3d0d5f60c5211151
Cezar Rosa <cecez86@gmail.com>
2021-05-13 18:19:52 -0300

    - category has many post's relationship - categories page - clockwork package

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
         @foreach($posts as $post)
             <article>
                 <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
-                <p><a href="#">{{ $post->category->name }}</a></p>
+                <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
                 <div>
                     {{ $post->excerpt }}
                 </div>
