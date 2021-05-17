05a1c1a3b888f1f819ea3ced36cfcc55b660f021
Cezar Rosa <cecez86@gmail.com>
2021-05-17 19:35:34 -0300

    - user and post relationship - uniqueness of category name and slug - db seeding - showing post author in the post page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // truncating tables
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        // creating records
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        $hobbies = Category::create([
+            'name' => 'Hobbies',
+            'slug' => 'hobbies'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My personal post',
+            'slug' => 'my-personal-post',
+            'excerpt' => 'bla bla bla',
+            'body' => 'Lorem ipsum dolor sit amet ametista.'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My work post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'bla bla bla 2',
+            'body' => 'Lorem ipsum dolor sit amet ametista.'
+        ]);
     }
 }
