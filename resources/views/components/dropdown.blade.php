6c092c60bb5b535ef76de0276aa1191367fe985f
Cezar Rosa <cecez86@gmail.com>
2021-06-10 14:28:08 -0300

    - small adjustments regarding viewing posts - increasing excerpt/body of a post in its factory

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -8,7 +8,7 @@
     </div>
 
     {{-- Links --}}
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl w-full z-50" style="display: none">
+    <div x-show="show" class="absolute bg-gray-100 max-h-52 mt-2 overflow-auto py-2 rounded-xl w-full z-50" style="display: none">
         {{ $slot }}
     </div>
 
