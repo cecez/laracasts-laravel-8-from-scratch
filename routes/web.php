979ff741b940686b4da8a4b432cc7a46a660127f
Cezar Rosa <cecez86@gmail.com>
2021-07-13 15:04:31 -0300

    - using mailchimp api to add members into an audience/list

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,21 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+// temporary code
+Route::get('/mailchimp', function() {
+   $listId = "086305bedb";
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+    $mailchimp->setConfig(['apiKey' => config('services.mailchimp.key'), 'server' => 'us20']);
+
+    $response = $mailchimp->lists->addListMember($listId, [
+        'email_address' => 'mailchimp2@cecez.com.br',
+        'status' => 'pending'   // it will generate a double opt-in confirmation (e-mail)
+    ]);
+
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::post('/posts/{post:slug}/comments', [PostCommentsController::class, 'store'])->middleware('auth');
